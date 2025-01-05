78586e6320b6ffc33135958bcb8680b692156ee0
Curry5004 <yunjin5004@naver.com>
2025-01-05 18:01:46 +0900

    토비 스프링 부트 강의 프로젝트 빌드

---
+++ src/test/java/tobyspringboot/helloboot/TobySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspringboot.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
